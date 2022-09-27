e571ae4d5139a3fd9349481e7b6c357dafd7cba6
Mike Li <mike.lxk@gmail.com>
2022-09-26 21:21:06 -0400

    up expedia

--- cpp/Interview/Expedia/findInteger.cpp
+++ cpp/Interview/Expedia/findInteger.cpp
@@ -3,25 +3,24 @@ vector<int> getGreatestElements(vector<int> arr, int k)
 {
     vector<int> res;
     int n = arr.size();
-    priority_queue<int> pq;
-    for (int i = 0; i < n; ++i) {
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (int i = 0; i < k; ++i) {
         pq.push(arr[i]);
-        if (i >= k - 1) {
-            vector<int> tmp;
-            for (int i = 0; i < k; ++i) {
-                tmp.push_back(pq.top());
-                pq.pop();
-            }
-            res.push_back(tmp.back());
-            for (auto &&i : tmp) {
-                pq.push(i);
-            }
+    }
+    int topK = pq.top();
+    res.push_back(topK);
+    for (int i = k; i < n; ++i) {
+        if (arr[i] > topK) {
+            pq.pop();
+            pq.push(arr[i]);
         }
+        topK = pq.top();
+        res.push_back(topK);
     }
     return res;
 }
 int main()
 {
     auto v = getGreatestElements({4, 2, 1, 3}, 2);
-    cout << v[0];
+    cout << v[0] << v[1] << v[2];
 }
--- js/Interview/Expedia/max-effi-same-sum.ts
+++ js/Interview/Expedia/max-effi-same-sum.ts
@@ -1,37 +1,25 @@
 /**
  * Given array of number, find pairs with the same sum,
  * there will only be 1 combination of the pair with same sum
- * Find sum of its efficiency : [a, b] = a * b 
- * @param skills 
+ * Find sum of its efficiency : [a, b] = a * b
+ * @param skills
  * @returns sum of effiency
  */
 function getEfficiency(skills: number[]) {
-  let m = new Map<number, [number, number][]>()
+  let effi = skills[0] + skills.at(-1)
   let n = skills.length
-  for (let i = 0; i < n; ++i) {
-    for (let j = i + 1; j < n; ++j) {
-      let [a, b] = [skills[i], skills[j]]
-      let sum = a + b
-      if (m.has(sum)) {
-        m.get(sum).push([a, b])
-      } else {
-        m.set(sum, [[a, b]])
-      }
+  let sum = 0
+  for (let l = 0; l < n; ++l) {
+    let r = n - l - 1
+    if (l >= r) {
+      break
     }
-  }
-  for (let v of m.values()) {
-    if (v.length >= n / 2) {
-      const set = new Set(v.flat())
-      if (set.size == v.length * 2) {
-        let sum = 0
-        for (const [a, b] of v) {
-          sum += a * b
-        }
-        return sum
-      }
+    if (skills[l] + skills[r] !== effi) {
+      return -1
     }
+    sum += skills[l] * skills[r]
   }
-  return -1
+  return sum
 }
 
 console.log(getEfficiency([5, 4, 2, 1]))
