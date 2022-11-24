732ee22132d052b2b34038ab73ab5ca2f365db63
mikelxk <mike.lxk@gmail.com>
2022-11-23 22:57:28 -0500

    up tree

--- TypeScript/Trees/106.construct-binary-tree-from-inorder-and-postorder-traversal.ts
+++ TypeScript/Trees/106.construct-binary-tree-from-inorder-and-postorder-traversal.ts
@@ -1,8 +1,8 @@
 function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
   if (!inorder.length && !postorder.length) return null
-  let root = new TreeNode(postorder.pop())
+  let root = new TreeNode(postorder.at(-1))
   let mid = inorder.indexOf(root.val)
   root.left = buildTree(inorder.slice(0, mid), postorder.slice(0, mid))
-  root.right = buildTree(inorder.slice(mid + 1), postorder.slice(mid))
+  root.right = buildTree(inorder.slice(mid + 1), postorder.slice(mid, -1))
   return root
 }
